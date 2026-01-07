4de60444c35a8d8513873feb9d8d597b50d07b4c
shynome <shynome@gmail.com>
2026-01-07 17:24:43 +0800

    登录后刷新

--- src/routes/(manager)/+layout.ts
+++ src/routes/(manager)/+layout.ts
@@ -1,10 +1,16 @@
 import { pb } from '$lib/pb'
 import { redirect } from '@sveltejs/kit'
 
-export async function load({ url }) {
+export async function load({ url, fetch }) {
+	let u = new URL('/login/', url)
+	u.searchParams.set('redirect', url.toString())
 	if (!pb.authStore.isValid) {
-		let u = new URL('/login', url)
-		u.searchParams.set('redirect', url.toString())
 		redirect(307, u)
 	}
+	await pb
+		.collection('_superusers')
+		.authRefresh({ fetch })
+		.catch(() => {
+			redirect(307, u)
+		})
 }
