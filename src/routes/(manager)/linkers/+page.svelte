a6ff3ed1d88775194c1915ebf065a03671efca23
shynome <shynome@gmail.com>
2026-02-25 15:59:30 +0800

    linkers 主页翻译ok

--- src/routes/(manager)/linkers/+page.svelte
+++ src/routes/(manager)/linkers/+page.svelte
@@ -50,6 +50,7 @@
 		}
 	})
 	import LinkerItem from './item.svelte'
+	import { m } from '$lib/paraglide/messages.js'
 </script>
 
 {#snippet actions()}
@@ -78,7 +79,7 @@
 		{/each}
 		<li class="list-row px-0">
 			<div></div>
-			<div class="label mx-auto">没有更多啦</div>
+			<div class="label mx-auto">{m.linkers_no_more()}</div>
 			<div></div>
 		</li>
 	</ul>
@@ -102,7 +103,7 @@
 						(err) => {
 							showToast({
 								color: 'error',
-								msg: `Linker更新出错: ${errStr(err)}`,
+								msg: `${m.linkers_modal_submit_failed()}: ${errStr(err)}`,
 							})
 						},
 					)
@@ -110,28 +111,28 @@
 		>
 			<h3 class="text-lg font-bold">
 				{#if linkerState.id}
-					更新信令中继器
+					{m.linkers_modal_update_title()}
 				{:else}
-					添加信令中继器
+					{m.linkers_modal_add_title()}
 				{/if}
 			</h3>
 			<fieldset class="fieldset">
-				<fieldset-legend class="fieldset-legend">称呼 (name)</fieldset-legend>
+				<fieldset-legend class="fieldset-legend">{m.linkers_name_input_title()}</fieldset-legend>
 				<input
 					name="name"
 					type="text"
 					class="input w-full"
-					placeholder="输入便于分辨的称呼"
+					placeholder={m.linkers_name_input_placeholder()}
 					bind:value={linkerState.name}
 					disabled={pending.value}
 				/>
-				<div class="label">未填写的话则会使用id来做显示</div>
+				<div class="label">{m.linkers_name_input_label()}</div>
 			</fieldset>
 			<fieldset class="fieldset">
 				<fieldset-legend class="fieldset-legend">
-					<span>中继器地址 (linker)*</span>
+					<span>{m.linkers_linker_input_title()}</span>
 					<div class="label">
-						停用
+						{m.linkers_linker_disable_btn()}
 						<input
 							name="disabled"
 							value="true"
@@ -145,28 +146,26 @@
 					name="linker"
 					type="url"
 					class="input w-full"
-					placeholder="https://linker.remoon.net/connect/xxx?token=yyyy"
+					placeholder={m.linkers_linker_input_placeholder()}
 					bind:value={linkerState.linker}
 					disabled={pending.value}
 					required
 				/>
 				<div class="label">
-					服务方提供给你的中继器连接地址
-					<br />
-					也支持 ssh 协议. ssh://user:pass@sshd.host:22/80/127.0.0.1
+					{@html m.linkers_linker_input_label()}
 				</div>
 			</fieldset>
 			<fieldset class="fieldset">
-				<fieldset-legend class="fieldset-legend">信令地址 (whip)</fieldset-legend>
+				<fieldset-legend class="fieldset-legend">{m.linkers_whip_input_title()}</fieldset-legend>
 				<input
 					name="whip"
 					type="text"
 					class="input w-full"
-					placeholder="https://linker.remoon.net/whip/xxx"
+					placeholder={m.linkers_whip_input_placeholder()}
 					bind:value={linkerState.whip}
 					disabled={pending.value}
 				/>
-				<div class="label">其他节点用来连接的信令地址</div>
+				<div class="label">{m.linkers_whip_input_label()}</div>
 			</fieldset>
 			<div class="modal-action">
 				<div class="flex-1">
@@ -179,18 +178,18 @@
 								deleteModalOpen = true
 							}}
 						>
-							删除
+							{m.linkers_modal_delete_btn_label()}
 						</button>
 					{/if}
 				</div>
 				<label for="linker-update-modal" class="btn btn-outline" class:btn-disabled={pending.value}>
-					取消
+					{m.linkers_modal_close_btn_label()}
 				</label>
 				<button type="submit" class="btn btn-primary" disabled={pending.value}>
 					{#if linkerState.id}
-						更新
+						{m.linkers_modal_update_btn_label()}
 					{:else}
-						添加
+						{m.linkers_modal_add_btn_label()}
 					{/if}
 				</button>
 			</div>
@@ -207,15 +206,15 @@
 />
 <div class="modal" role="dialog">
 	<div class="modal-box">
-		<h3 class="text-lg font-bold">Linker删除确认</h3>
+		<h3 class="text-lg font-bold">{m.linkers_del_modal_title()}</h3>
 		<p class="py-4 break-all">
-			你将要删除Linker:<br />
+			{m.linkers_del_modal_content()}:<br />
 			<b>{linkerState.name || linkerState.id}</b><br />
 			{linkerState.urls}
 		</p>
 		<div class="modal-action">
 			<label for="linker-delete-modal" class="btn btn-outline" class:btn-disabled={pending.value}>
-				取消
+				{m.linkers_del_modal_close_btn()}
 			</label>
 			<button
 				type="button"
@@ -232,14 +231,14 @@
 							(err) => {
 								showToast({
 									color: 'error',
-									msg: `Linker删除出错: ${errStr(err)}`,
+									msg: `${m.linkers_del_modal_submit_failed()}: ${errStr(err)}`,
 								})
 							},
 						)
 				}}
 				disabled={pending.value}
 			>
-				删除
+				{m.linkers_del_modal_submit_btn()}
 			</button>
 		</div>
 	</div>
